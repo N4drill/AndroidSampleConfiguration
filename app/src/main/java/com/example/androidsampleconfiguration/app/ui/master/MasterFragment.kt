2b3fdf2435c92bfdd96b2fbf8d748a932f4dc7bf
Nadrill <pawel.gluszczak1@gmail.com>
2020-04-06 19:05:41 +0200

    Adding rxJava, rxKotlin, rxAndroid and necessary extension

--- app/src/main/java/com/example/androidsampleconfiguration/app/ui/master/MasterFragment.kt
+++ app/src/main/java/com/example/androidsampleconfiguration/app/ui/master/MasterFragment.kt
@@ -1,11 +1,11 @@
 package com.example.androidsampleconfiguration.app.ui.master
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.androidsampleconfiguration.R
 
 class MasterFragment : Fragment() {
@@ -17,9 +17,11 @@ class MasterFragment : Fragment() {
 
     private lateinit var viewModel: MasterViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
